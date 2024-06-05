d5314937326cc9d3b62c6ab35d507fad6ac4d687
Wonders Victor <Wondersprince1@gmail.com>
2024-06-05 04:42:42 +0100

    Auth

---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+"""
+Basic auth module for the API
+"""
+import base64
+from api.v1.auth.auth import Auth
+
+
+class BasicAuth(Auth):
+	"""Basic auth"""
+	
+	def extract_base64_authorization_header(self,
+										    authorization_header: str) -> str:
+		"""returns the Base64 part of the Authorization header
+		for a Basic Authentication:"""
+		return None if (authorization_header is None
+				  		or type(authorization_header) is not str
+						or not authorization_header.startswith("Basic "))\
+						else authorization_header.split("Basic")[-1].strip()
+	
+	def decode_base64_authorization_header(self,
+										   base64_authorization_header: str) -> str:
+		"""returns the decoded value of a Base64 string base64_authorization_header"""
+		if (base64_authorization_header is None
+	  		or type(base64_authorization_header) is not str):
+			return None
+		try:
+			decoded = base64.b64decode(base64_authorization_header)
+			return decoded.decode("utf-8")
+		except BaseException:
+			return None
