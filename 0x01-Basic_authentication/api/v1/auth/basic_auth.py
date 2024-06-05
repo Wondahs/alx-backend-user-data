f7518a6bedbbe18f44769bb516869cb50a6df0af
Wonders Victor <Wondersprince1@gmail.com>
2024-06-05 04:47:33 +0100

    v

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -4,6 +4,7 @@ Basic auth module for the API
 """
 import base64
 from api.v1.auth.auth import Auth
+from typing import Tuple
 
 
 class BasicAuth(Auth):
@@ -29,3 +30,9 @@ class BasicAuth(Auth):
 			return decoded.decode("utf-8")
 		except BaseException:
 			return None
+		
+	def extract_user_credentials(self, decoded_base64_authorization_header: str) -> Tuple[str, str]:
+		"""in the class BasicAuth that returns the user email and password
+		from the Base64 decoded value."""
+		return (None, None) if (decoded_base64_authorization_header is None
+						        or type())
