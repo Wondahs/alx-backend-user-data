5344f106bbf4613ced0baff4f3616f02e785adc2
Wonders Victor <wondersprince1@gmail.com>
2024-06-05 10:28:00 +0000

    Fixes

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -7,23 +7,27 @@ from typing import TypeVar, List
 
 
 class Auth:
-	"""Auth class"""
-	def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-		"""
-		returns True if the path is not in the list of strings excluded_paths
-		"""
-		if path is None or excluded_paths in [[], None]:
-			return True
-		path = path if path.endswith('/') else path + '/'
-		return path not in excluded_paths
-	
-	def authorization_header(self, request=None) -> str:
-		""" validate all requests to secure the API"""
-		if request is None:
-			return None
-		return request.headers.get("Authorization")
-	
-	def current_user(self, request=None) -> TypeVar('User'):
-		"""returns None"""
-		return None
-		
+    """Auth class"""
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """
+        returns True if the path is not in the list of strings excluded_paths
+        """
+        if path is None or excluded_paths in [[], None]:
+            return True
+        if not path.endswith("*"):
+            path = path if path.endswith('/') else path + '/'
+            return path not in excluded_paths
+        else:
+            path = path.replace("*", "")
+            return not all(path in ex_path for ex_path in excluded_paths)
+
+    def authorization_header(self, request=None) -> str:
+        """ validate all requests to secure the API"""
+        if request is None:
+            return None
+        return request.headers.get("Authorization")
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """returns None"""
+        return None
+
--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -4,46 +4,46 @@ Basic auth module for the API
 """
 import base64
 from api.v1.auth.auth import Auth
-from typing import Tuple
+from typing import Tuple, TypeVar
 
 
 class BasicAuth(Auth):
-	"""Basic auth"""
-	
-	def extract_base64_authorization_header(self,
-										    authorization_header: str) -> str:
-		"""returns the Base64 part of the Authorization header
-		for a Basic Authentication:"""
-		return None if (authorization_header is None
-				  		or type(authorization_header) is not str
-						or not authorization_header.startswith("Basic "))\
-						else authorization_header.split("Basic")[-1].strip()
-	
-	def decode_base64_authorization_header(self,
-										   base64_authorization_header: str) -> str:
-		"""returns the decoded value of a Base64 string base64_authorization_header"""
-		if (base64_authorization_header is None
-	  		or type(base64_authorization_header) is not str):
-			return None
-		try:
-			decoded = base64.b64decode(base64_authorization_header)
-			return decoded.decode("utf-8")
-		except BaseException:
-			return None
-		
-	def extract_user_credentials(self,
+    """Basic auth"""
+
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
+        """returns the Base64 part of the Authorization header
+        for a Basic Authentication:"""
+        return None if (authorization_header is None
+                or type(authorization_header) is not str
+                or not authorization_header.startswith("Basic "))\
+                        else authorization_header.split("Basic")[-1].strip()
+
+    def decode_base64_authorization_header(self,
+                                           base64_authorization_header: str) -> str:
+        """returns the decoded value of a Base64 string base64_authorization_header"""
+        if (base64_authorization_header is None
+                or type(base64_authorization_header) is not str):
+            return None
+        try:
+            decoded = base64.b64decode(base64_authorization_header)
+            return decoded.decode("utf-8")
+        except BaseException:
+            return None
+
+    def extract_user_credentials(self,
                                  decoded_base64_authorization_header: str) -> Tuple[str, str]:
-		"""in the class BasicAuth that returns the user email and password
-		from the Base64 decoded value."""
-		return (None, None) if (decoded_base64_authorization_header is None
-						        or type(decoded_base64_authorization_header) is not str
-                                or ":" not in decoded_base64_authorization_header)\
-                                        else tuple(decoded_base64_authorization_header.split(":"))
+        """in the class BasicAuth that returns the user email and password
+        from the Base64 decoded value."""
+        return (None, None) if (decoded_base64_authorization_header is None
+                or type(decoded_base64_authorization_header) is not str
+                or ":" not in decoded_base64_authorization_header)\
+                        else tuple(decoded_base64_authorization_header.split(":", 1))
 
     def user_object_from_credentials(self,
                                      user_email: str,
                                      user_pwd: str) -> TypeVar('User'):
-        """in the class BasicAuth that returns the User instance based on his email and password.
+        """returns the User instance based on his email and password.
         Args:
             self (_type_): Basic auth instance
             user_email(str): user email
@@ -63,7 +63,6 @@ class BasicAuth(Auth):
                 return user
 
         return None
-
     def current_user(self, request=None) -> TypeVar('User'):
         """get current user"""
         try:
