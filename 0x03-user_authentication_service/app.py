db9b365a5cc58caf97f1f616078ce9770a71d44c
Wonders Victor <Wondersprince1@gmail.com>
2024-06-10 23:51:10 +0100

    Working...

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,1 +1,45 @@
+#!/usr/bin/env python3
+"""Flask app"""
+from flask import Flask, jsonify, request, abort
+from auth import Auth
 
+
+AUTH = Auth()
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def home():
+    """return a JSON payload of the form:
+    {"message": "Bienvenue")
+    """
+    return jsonify({"message": "Bienvenue"})
+
+@app.route("/users", methods=["POST"], strict_slashes=False)
+def register_user():
+    """End-point to register a user"""
+    email = request.form.get("email")
+    password = request.form.get("password")
+    
+    try:
+        AUTH.register_user(email, password)
+        return jsonify({"email": f"{email}", "message": "user created"})
+    except ValueError:
+         return jsonify({"message": "email already registered"}), 400
+
+@app.route("/sessions", methods=["POST"], strict_slashes=False)
+def login():
+    """Login session"""
+    email = request.form.get("email")
+    password = request.form.get("password")
+    if AUTH.valid_login(email, password):
+        session_id = AUTH.create_session(email)
+        response = jsonify({"email": f"{email}", "message": "logged in"})
+        response.set_cookie("session_id", session_id)
+        return response
+    else:
+        abort(401)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -3,7 +3,7 @@
 import bcrypt
 from db import DB
 from user import User
-from typing import TypeVar
+from typing import TypeVar, Union
 from sqlalchemy.orm.exc import NoResultFound
 
 
@@ -24,6 +24,32 @@ class Auth:
         except NoResultFound:
             hashed_password = _hash_password(password)
             return self._db.add_user(email, hashed_password)
+    
+    def valid_login(self, email: str, password: str) -> bool:
+        """Validates user credentials"""
+        try:
+            user = self._db.find_user_by(email=email)
+        except NoResultFound:
+            return False
+        if not bcrypt.checkpw(password.encode("utf-8"), user.hashed_password):
+            return False
+        return True
+    
+    def create_session(self, email: str) -> str:
+        """takes an email string argument
+        and returns the session ID as a string."""
+        try:
+            user = self._db.find_user_by(email=email)
+            session_id = _generate_uuid()
+            self._db.update_user(user.id, session_id=session_id)
+            return session_id
+        except NoResultFound:
+            return None
+    
+    def get_user_from_session_id(self, session_id: str) -> Union[TypeVar("User"), None]:
+        """takes a single session_id string argument
+        and returns the corresponding User or None."""
+        
 
 
 def _hash_password(password: str) -> bytes:
@@ -34,3 +60,9 @@ def _hash_password(password: str) -> bytes:
     salt = bcrypt.gensalt()
     hashed_password = bcrypt.hashpw(password_bytes, salt)
     return hashed_password
+
+def _generate_uuid() -> str:
+    """GEnerates a new UUID"""
+    from uuid import uuid4
+    
+    return str(uuid4())
