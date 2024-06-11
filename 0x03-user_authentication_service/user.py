5513f111952e5b4683b3781e69c02ec4451d6cc9
Wonders Victor <wondersprince1@gmail.com>
2024-06-11 22:01:15 +0000

    More corrections

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -10,7 +10,7 @@ Base = declarative_base()
 
 class User(Base):
     """User Model Class"""
-    __tablename__ = "user"
+    __tablename__ = "users"
     id = Column(Integer, primary_key=True)
     email = Column(String(250), nullable=False)
     hashed_password = Column(String(250), nullable=False)
